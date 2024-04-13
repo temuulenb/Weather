3e02f7c7ba0843434ae748ff30cdd850ba894ea0
temuulenb <temuu1109@gmail.com>
2024-04-13 00:44:38 -0700

    city search

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const getApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9f0b7204demsh3e1b46896435bbdp1ddc66jsne899fcb3ccb8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
